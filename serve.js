00d629c2061bc476bcd62ed4febf6a6a6d510bc7
podmaxs <podmaxs@gmail.com>
2017-10-23 07:53:36 -0300

    actualizacion de ejemplo

--- serve.js
+++ serve.js
@@ -3,13 +3,11 @@
 
 let app    = require('./dist/module');
 	
-let router = new app.router('sample');
 	//session = require('draweb-nrm-session');
 
-	//router.addVerb('get', '/sample','getSample');
 new app.serve(new app.config({
 	appName: "Sample draweb-nrm",
 	apikey:  "myapikey"
 }), [
 	//session
-]).run([router]);
+]).run(['sample']);
